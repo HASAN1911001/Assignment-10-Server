6e2303f1b8cb0306b48da375901f1afe088d7e2a
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-10-26 21:05:16 +0600

    done

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const categories = require('./data/categories.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/course-categories', (req, res) => {
+  res.send(categories)
+})
+
+app.get('/courses/1', (req, res) => {
+  res.send(categories)
 })
 
 app.listen(port, () => {
